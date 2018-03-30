840226cb08826d8a1087dfe61c3b783766db70fb
Ruphaa <ruphaaganesh@gmail.com>
2018-03-30 17:32:14 +0530

    Removed registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import CircularProgressBar from './components/CircularProgressBar';
 
 var destination = document.querySelector('#root');
